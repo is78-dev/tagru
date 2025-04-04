7c9fcff8857830ba28847a1b2f8871660d0ef44d
is1-coder <isoshi.desktop@gmail.com>
2025-04-05 00:07:29 +0900

    add: LP、ログインページ作成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,12 @@ import { GoogleLoginButton } from "@/app/login/google-login-button";
 
 export default function Page() {
   return (
-    <main className="grid min-h-dvh place-items-center bg-muted p-4">
-      <div className="max-w-md space-y-6 rounded-xl border bg-background p-8 shadow">
-        <h1 className="text-3xl font-bold">Login</h1>
-        <p className="text-muted-foreground">
-          新規登録、ログインのどちらも以下から行うことができます。
-        </p>
-        <GoogleLoginButton />
-      </div>
-    </main>
+    <div className="mx-auto max-w-xl py-20">
+      <h1 className="mb-3 text-4xl font-extrabold">Login</h1>
+      <p className="mb-8 text-muted-foreground">
+        新規登録、ログインのどちらも以下から行うことができます。
+      </p>
+      <GoogleLoginButton />
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight, FolderTree, NotebookPen, Tag } from "lucide-react";
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <main className="grid min-h-dvh place-items-center bg-muted p-4">
-      <div className="max-w-md space-y-6 rounded-xl border bg-background p-8 shadow">
-        <h1 className="text-3xl font-bold">Tagru</h1>
-        <p className="text-muted-foreground">
-          Tagruは動画などweb上のお気に入りのコンテンツをタグ付けして管理するサービスです。
+    <div className="mx-auto py-6">
+      <section className="flex flex-col items-center py-40">
+        <h1 className="mb-3 text-4xl font-extrabold">Organize with tags.</h1>
+        <p className="mb-8 text-muted-foreground">
+          Tagruはyoutube上の動画にタグをつけて管理できるオンラインサービスです。
+          <br />
+          動画ごとのメモや、タグの階層化により、自分だけの動画データベースを作成できます。
         </p>
-      </div>
-    </main>
+        <Button className="rounded-full p-4 px-6 text-lg" asChild>
+          <Link href="/login">
+            ログイン
+            <ArrowRight className="size-5" />
+          </Link>
+        </Button>
+      </section>
+
+      <section className="flex flex-wrap justify-center gap-10">
+        <div className="w-64">
+          <h2 className="mb-2 text-xl font-bold">
+            <Tag className="mr-2 inline-block size-5" />
+            タグで分類
+          </h2>
+          <p className="text-muted-foreground">
+            動画に自由なタグを付けて、
+            <br />
+            自分だけのルールで分類できます。
+          </p>
+        </div>
+        <div className="w-64">
+          <h2 className="mb-2 text-xl font-bold">
+            <NotebookPen className="mr-2 inline-block size-5" />
+            メモを残せる
+          </h2>
+          <p className="text-muted-foreground">
+            動画やタグにメモを追加して、
+            <br />
+            内容を補足できます。
+          </p>
+        </div>
+        <div className="w-64">
+          <h2 className="mb-2 text-xl font-bold">
+            <FolderTree className="mr-2 inline-block size-5" />
+            親子タグで階層管理
+          </h2>
+          <p className="text-muted-foreground">
+            タグに親子関係を設定できるので、
+            <br />
+            タグ同士のリンクが可能です。
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }
